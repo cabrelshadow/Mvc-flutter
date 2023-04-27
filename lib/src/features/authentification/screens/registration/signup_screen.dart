b16dcd91f33b2981ef62a0754228603385650677
cabrel sianou <sianou@gamail.com>
2023-04-27 09:28:24 +0100

    added registration page

---
+++ lib/src/features/authentification/screens/registration/signup_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: const [
+            Align(
+              alignment: Alignment.topCenter,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
